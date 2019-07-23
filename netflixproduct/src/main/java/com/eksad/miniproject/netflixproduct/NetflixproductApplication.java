5c01b68776a8b54032eeed6ca48f42872c54e659
rashadhrrs <rashadhrrs@gmail.com>
2019-07-23 08:17:48 +0700

    Circuit Breaker

--- netflixproduct/src/main/java/com/eksad/miniproject/netflixproduct/NetflixproductApplication.java
+++ netflixproduct/src/main/java/com/eksad/miniproject/netflixproduct/NetflixproductApplication.java
@@ -2,8 +2,11 @@ package com.eksad.miniproject.netflixproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+
+@EnableCircuitBreaker
 public class NetflixproductApplication {
 
 	public static void main(String[] args) {
--- netflixproduct/src/main/java/com/eksad/miniproject/netflixproduct/controller/ProductController.java
+++ netflixproduct/src/main/java/com/eksad/miniproject/netflixproduct/controller/ProductController.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,71 +23,90 @@ import org.springframework.web.client.RestTemplate;
 import com.eksad.miniproject.netflixproduct.dao.ProductDao;
 import com.eksad.miniproject.netflixproduct.model.Brand;
 import com.eksad.miniproject.netflixproduct.model.Product;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@Service
 @RestController
 @RequestMapping(value = "/product")
 public class ProductController {
-	
-	@Autowired 
+
+	@Autowired
 	ProductDao productDao;
+
+	@GetMapping("getAll")
+	public List<Product> getAll() {
+
+		List<Product> result = new ArrayList<>();
+
+		productDao.findAll().forEach(result::add);
+
+		return result;
+	}
+
+	@GetMapping("getAllBrand")
+	/*
+	 * public List<Brand> getAllBrand() { RestTemplate restTemplate = new
+	 * RestTemplate(); ResponseEntity<List<Brand>> response =
+	 * restTemplate.exchange("http://localhost:8081/brand/getAll", HttpMethod.GET,
+	 * null, new ParameterizedTypeReference<List<Brand>>() { }); List<Brand> brand =
+	 * response.getBody();
+	 * 
+	 * return brand; }
+	 */
+
 	
+	  @HystrixCommand(fallbackMethod = "fallbackList" ) 
+	  public List <Brand> getAllBrand() { 
+	  RestTemplate restTemplate = new RestTemplate();
+	  ResponseEntity<List<Brand>> response = restTemplate.exchange(
+	  "http://localhost:8081/brand/getAll", HttpMethod.GET, null, new
+	  ParameterizedTypeReference<List<Brand>>(){}); List<Brand> brand =
+	  response.getBody();
+	  
+	  return getAllBrand(); 
+	  
+	}
+	 
+
 	
-	  @GetMapping("getAll") 
-	  public List <Product> getAll() {
+	  @SuppressWarnings("unused") 
+	  public String fallbackList() {
 	  
+	  System.out.println("Service is down!!! fallback route enabled...");
 	  
-	  List<Product>result = new ArrayList<>();
-	  
-	  productDao.findAll().forEach(result::add);
-	  
-	  return result; }
+	  return "Circuit breaker enabled"; }
 	 
 	
-	@GetMapping("getAllBrand")
-	public List <Brand> getAllBrand() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<List<Brand>> response = restTemplate.exchange(
-		  "http://localhost:8081/brand/getAll",
-		  HttpMethod.GET,
-		  null,
-		  new ParameterizedTypeReference<List<Brand>>(){});
-		List<Brand> brand = response.getBody();
-		
-		return brand;
-		}
-	
-	
+
 	@PostMapping(value = "save")
-	public Product save(@RequestBody Product product) { 
+	public Product save(@RequestBody Product product) {
 		try {
-			
+
 			return productDao.save(product);
 		} catch (Exception e) {
-		
+
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	@PutMapping(value ="update/{id}")
+
+	@PutMapping(value = "update/{id}")
 	public Product update(@RequestBody Product product, @PathVariable Long id) {
-		
+
 		Product productSelected = productDao.findById(id).orElse(null);
-		if (productSelected!= null) {
+		if (productSelected != null) {
 			productSelected.setName(product.getName());
 			productSelected.setPrice(product.getPrice());
-			
+
 			productDao.save(productSelected);
 			return productDao.save(productSelected);
-		}
-		else {
+		} else {
 			return null;
 		}
-		
-				
+
 	}
-	
-	@DeleteMapping(value ="delete/{id}")
+
+	@DeleteMapping(value = "delete/{id}")
 	public HashMap<String, Object> delete(@PathVariable Long id) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
 		productDao.deleteById(id);
